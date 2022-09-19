1cb13fe3328d6c84cdbaf80cf3e1a25f9dda910b
Mayra Zurita <mizrmx@gmail.com>
2022-09-19 09:40:04 -0500

    Update server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+
+const app = express()
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+console.log(join(_dirname, 'views'))
+
+app.set('views', join(_dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(3000)
+console.log('Server is listening on port', 3000)

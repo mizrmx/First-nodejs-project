2c7bd3b354f47d6196b7bd06709889afe06d5550
Mayra Zurita <mizrmx@gmail.com>
2022-09-20 09:21:44 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ console.log(join(_dirname, 'views'))
 
 app.set('views', join(_dirname, 'views'))
 app.set('view engine', 'ejs')
+
 app.use(indexRoutes)
+
+app.use(express.static(join(_dirname, 'public')))
 
 app.listen(3000)
 console.log('Server is listening on port', 3000)
